7d99d7f88f75c898be4d89e87ae54870cdd1e6ed
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-20 22:14:09 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,21 +1,19 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/) 
-  else
-  end
+  word.match(/^[AaEeIiOoUu]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/^un+\w+ing/)
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
- 
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-# (\d+)-9
+  phone.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
 end
