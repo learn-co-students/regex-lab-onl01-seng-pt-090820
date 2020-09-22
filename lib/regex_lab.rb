d044f4fcba12dfb2bbb4899bb64eb8d361d158a1
DavidRMorphew <morphewd@umich.edu>
2020-09-22 17:55:05 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
