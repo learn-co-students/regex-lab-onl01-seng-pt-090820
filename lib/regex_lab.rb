47a7fe7ce3586ef35a78669d6716c2df4bc2e1b5
DavidRMorphew <morphewd@umich.edu>
2020-09-23 11:51:07 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,13 +8,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+     !!text.match(/^[A-Z].+[.?!]$/)
 end
 
 def valid_phone_number?(phone)
-
+    !!phone.match(/(\D\d{3}\D|\d{3})(\d{3}|\D\d{3})(\d{4}|\D\d{4})\z/)
 end
